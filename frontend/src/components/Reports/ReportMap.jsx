edfcfbaa88c989788e3b1c3ae898a28ad5eee1bf
CliffLinus Thomi . <thomilinus5@gmail.com>
2025-07-31 22:43:47 +0300

    Update ReportMap.jsx

--- frontend/src/components/Reports/ReportMap.jsx
+++ frontend/src/components/Reports/ReportMap.jsx
@@ -1,3 +1,26 @@
+// import React from 'react';
+// import { Map, Marker } from '@vis.gl/react-google-maps';
+
+// const ReportMap = ({ lat, lng }) => {
+//   const position = { lat: lat, lng: lng };
+
+//   return (
+//     <div style={{ height: '200px', width: '100%', marginTop: '1rem' }}>
+//       <Map
+//         defaultCenter={position}
+//         defaultZoom={14}
+//         gestureHandling={'greedy'}
+//         disableDefaultUI={true}
+//       >
+//         <Marker position={position} />
+//       </Map>
+//     </div>
+//   );
+// };
+
+// export default ReportMap;
+
+// ReportMap.jsx
 import React from 'react';
 import { Map, Marker } from '@vis.gl/react-google-maps';
 
@@ -5,7 +28,7 @@ const ReportMap = ({ lat, lng }) => {
   const position = { lat: lat, lng: lng };
 
   return (
-    <div style={{ height: '200px', width: '100%', marginTop: '1rem' }}>
+    <div className="h-64 w-full mt-4 rounded-lg overflow-hidden shadow-lg">
       <Map
         defaultCenter={position}
         defaultZoom={14}
