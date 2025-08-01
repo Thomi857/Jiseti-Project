46506519b46fb82d3b067393989ea9c058f96a72
Luke <luke.wangai@student.moringaschool.com>
2025-08-02 02:03:41 +0300

    stage 25 test 2 maps

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'leaflet/dist/leaflet.css';
 import App from './App.jsx'
 import './index.css'
 // src/main.jsx or src/index.css
-import 'leaflet/dist/leaflet.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
