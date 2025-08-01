c9cbfbc9b76665f2ff76ab4120c62d47b3217e4c
Luke <luke.wangai@student.moringaschool.com>
2025-08-01 21:03:36 +0300

    stage 15 /api route

--- backend/app.py
+++ backend/app.py
@@ -38,6 +38,8 @@ app.register_blueprint(reports_bp, url_prefix='/api')
 if __name__ == '__main__':
     if init_db():
         logging.info("✅ Database initialized successfully.")
+        for rule in app.url_map.iter_rules():
+            print(f"{rule.methods} -> {rule}")
         app.run(debug=True, port=5000)
     else:
         logging.error("❌ Failed to initialize the database.")
