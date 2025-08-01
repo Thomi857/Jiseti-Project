b14795f46034c097c9a8216649579e4bf8071198
Luke <luke.wangai@student.moringaschool.com>
2025-08-02 01:07:51 +0300

    stage 23

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // ✅ Add this line to set correct base path
   plugins: [react()],
 })
